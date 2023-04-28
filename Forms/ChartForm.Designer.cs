950eb91ed087bc4d19e374be63f366121d3add22
akoot <akoot@akoot.me>
2023-04-27 22:44:47 -0400

    going to ruin the UI

--- Forms/ChartForm.Designer.cs
+++ Forms/ChartForm.Designer.cs
@@ -33,7 +33,13 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChartForm));
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
@@ -52,20 +58,50 @@
             series1.Name = "Series1";
             series1.YValuesPerPoint = 4;
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(800, 450);
+            this.chart1.Size = new System.Drawing.Size(753, 450);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chart1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(943, 450);
+            this.splitContainer1.SplitterDistance = 753;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(2, 144);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(184, 30);
+            this.comboBox1.TabIndex = 0;
             // 
             // ChartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(943, 450);
+            this.Controls.Add(this.splitContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ChartForm";
             this.Text = "StocksEnjoyer 3.0 - Chart";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,5 +109,7 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Forms/SettingsForm.Designer.cs
+++ Forms/SettingsForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
             this.checkBox_filter = new System.Windows.Forms.CheckBox();
             this.panel_filter = new System.Windows.Forms.Panel();
@@ -37,17 +38,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button_load = new System.Windows.Forms.Button();
             this.comboBox_csvList = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.button_reload = new System.Windows.Forms.Button();
+            this.toolTip_reload = new System.Windows.Forms.ToolTip(this.components);
             this.panel_filter.SuspendLayout();
             this.SuspendLayout();
             // 
             // checkBox_filter
             // 
+            this.checkBox_filter.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.checkBox_filter.AutoSize = true;
+            this.checkBox_filter.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
+            this.checkBox_filter.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(117)))), ((int)(((byte)(255)))));
             this.checkBox_filter.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.checkBox_filter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox_filter.Location = new System.Drawing.Point(47, 119);
+            this.checkBox_filter.Location = new System.Drawing.Point(140, 282);
             this.checkBox_filter.Name = "checkBox_filter";
             this.checkBox_filter.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.checkBox_filter.Size = new System.Drawing.Size(186, 27);
@@ -58,14 +62,16 @@
             // 
             // panel_filter
             // 
-            this.panel_filter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_filter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel_filter.AutoSize = true;
+            this.panel_filter.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.panel_filter.Controls.Add(this.radioButton_daily);
             this.panel_filter.Controls.Add(this.radioButton_weekly);
             this.panel_filter.Controls.Add(this.radioButton_monthly);
             this.panel_filter.Enabled = false;
-            this.panel_filter.Location = new System.Drawing.Point(283, 84);
+            this.panel_filter.Location = new System.Drawing.Point(140, 303);
             this.panel_filter.Name = "panel_filter";
-            this.panel_filter.Size = new System.Drawing.Size(409, 62);
+            this.panel_filter.Size = new System.Drawing.Size(287, 33);
             this.panel_filter.TabIndex = 7;
             // 
             // radioButton_daily
@@ -73,7 +79,7 @@
             this.radioButton_daily.AutoSize = true;
             this.radioButton_daily.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.radioButton_daily.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton_daily.Location = new System.Drawing.Point(66, 18);
+            this.radioButton_daily.Location = new System.Drawing.Point(3, 3);
             this.radioButton_daily.Name = "radioButton_daily";
             this.radioButton_daily.Size = new System.Drawing.Size(76, 27);
             this.radioButton_daily.TabIndex = 3;
@@ -87,7 +93,7 @@
             this.radioButton_weekly.AutoSize = true;
             this.radioButton_weekly.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.radioButton_weekly.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton_weekly.Location = new System.Drawing.Point(148, 18);
+            this.radioButton_weekly.Location = new System.Drawing.Point(85, 3);
             this.radioButton_weekly.Name = "radioButton_weekly";
             this.radioButton_weekly.Size = new System.Drawing.Size(95, 27);
             this.radioButton_weekly.TabIndex = 4;
@@ -101,7 +107,7 @@
             this.radioButton_monthly.AutoSize = true;
             this.radioButton_monthly.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.radioButton_monthly.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton_monthly.Location = new System.Drawing.Point(249, 18);
+            this.radioButton_monthly.Location = new System.Drawing.Point(186, 3);
             this.radioButton_monthly.Name = "radioButton_monthly";
             this.radioButton_monthly.Size = new System.Drawing.Size(98, 27);
             this.radioButton_monthly.TabIndex = 5;
@@ -112,18 +118,20 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(445, 256);
+            this.label1.Location = new System.Drawing.Point(136, 339);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 22);
+            this.label1.Size = new System.Drawing.Size(75, 22);
             this.label1.TabIndex = 1;
-            this.label1.Text = "File Name";
+            this.label1.Text = "CSV file";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button_load
             // 
+            this.button_load.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button_load.AutoSize = true;
-            this.button_load.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.button_load.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(188)))), ((int)(((byte)(212)))));
             this.button_load.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.button_load.FlatAppearance.BorderSize = 0;
@@ -132,10 +140,12 @@
             this.button_load.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.button_load.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_load.ForeColor = System.Drawing.Color.White;
-            this.button_load.Location = new System.Drawing.Point(47, 340);
+            this.button_load.Image = ((System.Drawing.Image)(resources.GetObject("button_load.Image")));
+            this.button_load.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_load.Location = new System.Drawing.Point(140, 400);
             this.button_load.Name = "button_load";
             this.button_load.Padding = new System.Windows.Forms.Padding(10);
-            this.button_load.Size = new System.Drawing.Size(84, 51);
+            this.button_load.Size = new System.Drawing.Size(445, 51);
             this.button_load.TabIndex = 8;
             this.button_load.Text = "Load";
             this.button_load.UseVisualStyleBackColor = false;
@@ -143,42 +153,38 @@
             // 
             // comboBox_csvList
             // 
+            this.comboBox_csvList.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.comboBox_csvList.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.comboBox_csvList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_csvList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox_csvList.FormattingEnabled = true;
-            this.comboBox_csvList.Location = new System.Drawing.Point(173, 219);
+            this.comboBox_csvList.Location = new System.Drawing.Point(140, 364);
             this.comboBox_csvList.Name = "comboBox_csvList";
-            this.comboBox_csvList.Size = new System.Drawing.Size(184, 24);
+            this.comboBox_csvList.Size = new System.Drawing.Size(312, 30);
             this.comboBox_csvList.TabIndex = 0;
             this.comboBox_csvList.SelectedIndexChanged += new System.EventHandler(this.comboBox_csvList_Changed);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ButtonShadow;
-            this.label2.Location = new System.Drawing.Point(25, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(177, 31);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "File Selection";
-            // 
             // button_reload
             // 
+            this.button_reload.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button_reload.AutoSize = true;
             this.button_reload.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button_reload.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.button_reload.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(133)))), ((int)(((byte)(255)))));
             this.button_reload.FlatAppearance.BorderSize = 0;
-            this.button_reload.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
-            this.button_reload.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(119)))), ((int)(((byte)(202)))));
-            this.button_reload.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button_reload.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(133)))), ((int)(((byte)(255)))));
+            this.button_reload.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(255)))));
+            this.button_reload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_reload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_reload.ForeColor = System.Drawing.Color.White;
-            this.button_reload.Location = new System.Drawing.Point(520, 174);
+            this.button_reload.Image = ((System.Drawing.Image)(resources.GetObject("button_reload.Image")));
+            this.button_reload.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_reload.Location = new System.Drawing.Point(454, 364);
             this.button_reload.Name = "button_reload";
-            this.button_reload.Size = new System.Drawing.Size(140, 26);
+            this.button_reload.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button_reload.Size = new System.Drawing.Size(131, 30);
             this.button_reload.TabIndex = 10;
-            this.button_reload.Text = "Reload Stock Data";
+            this.button_reload.Text = "Reload Folder";
+            this.button_reload.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolTip_reload.SetToolTip(this.button_reload, "Reload Stock Data");
             this.button_reload.UseVisualStyleBackColor = false;
             this.button_reload.Click += new System.EventHandler(this.button_reload_Click);
             // 
@@ -187,9 +193,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(750, 624);
             this.Controls.Add(this.button_reload);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox_csvList);
             this.Controls.Add(this.checkBox_filter);
             this.Controls.Add(this.button_load);
@@ -215,8 +220,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button_load;
         private System.Windows.Forms.ComboBox comboBox_csvList;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button_reload;
+        private System.Windows.Forms.ToolTip toolTip_reload;
     }
 }
 
