a8f3428d19e0fa64d8ffd803caa99a5430a17596
akoot <akoot@akoot.me>
2023-04-30 15:55:01 -0400

    Commented PatternRecognizer.cs

--- Recognizers/PatternRecognizer.cs
+++ Recognizers/PatternRecognizer.cs
@@ -2,8 +2,19 @@
 
 namespace StocksEnjoyer
 {
+    /// <summary>
+    /// A Pattern Recognizer class is meant to be used as an abstract class which implements the function
+    /// "Draw". I chose "Draw" instead of recognize because it would be easier and more straightforward
+    /// to draw the actual patterns directly rather than first getting the indices.
+    /// </summary>
     public abstract class PatternRecognizer
     {
+        /// <summary>
+        /// This function takes in the Chart object and makes changes to the relevant data points.
+        /// For now, they will color each point depending on what pattern it is.
+        /// It will also create a new legend item showing what color it is.
+        /// </summary>
+        /// <param name="chart"></param>
         public abstract void Draw(Chart chart);
     }
 }
